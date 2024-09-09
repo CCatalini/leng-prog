f544677605cbe6b3dfce8b0a6c21bd844ce7f250
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-09-09 16:03:58 -0300

    frequencies

--- haskell-exercises/frequencies/src/Frequencies.hs
+++ haskell-exercises/frequencies/src/Frequencies.hs
@@ -6,14 +6,26 @@ import Data.Tuple(swap)
 
 type Frequency = (Int, Char)
 
-frequencies::String -> [Frequency]
-frequencies = error "Implement it"
+frequencies :: String -> [Frequency]
+frequencies str = insertionSort $ map swap $ Map.toList $ frequencyMap str
 
+
+-- El constraint (Ord a) indica que el tipo a debe ser ordenable
+-- Map a Int = [key=elem, value=frequency]
+-- foldr recorre la lista de derecha a izquierda, y va acumulando el resultado en el segundo argumento de la función lambda
+-- Map.insertWith (+) x 1 acc: si x no está en el map, lo inserta con valor 1, si ya está, le suma 1 al valor que ya tiene
 frequencyMap::(Ord a) => [a] -> Map a Int
-frequencyMap = error "Implement it"
+frequencyMap = foldr (\x acc -> Map.insertWith (+) x 1 acc) Map.empty
 
-insert::(Ord a) => a -> [a] -> [a]
-insert = error "Implement it"
+
+-- inserta un elemento en una lista ordenada
+insert :: (Ord a) => a -> [a] -> [a]
+insert x [] = [x]
+insert x (y:ys)
+    | x <= y    = x : y : ys
+    | otherwise = y : insert x ys
+
 
 insertionSort :: (Ord a) => [a] -> [a]
-insertionSort = error "Implement it"
+insertionSort [] = []
+insertionSort (x:xs) = insert x (insertionSort xs)
--- haskell-exercises/frequencies/src/Trie.hs
+++ haskell-exercises/frequencies/src/Trie.hs
@@ -7,17 +7,41 @@ data Trie a = Leaf a
 
 -- Bring definition from previous TP
 
-left::Trie a -> Trie a
-left = error "Define it"
+left :: Trie a -> Trie a
+left (Node l _) = l
+left (Leaf _) = error "Left of: Leaf"
 
-right::Trie a -> Trie a
-left = error "Define it"
-  
-find::Bits -> Trie a -> a
-find = error "Define it"
 
-decode::Bits -> Trie Char -> String
-decode = error "Define it"
+right :: Trie a -> Trie a
+right (Node _ r) = r
+right (Leaf _) = error "Right of: Leaf"
 
-toList::Trie a -> [(a, Bits)]
-toList = error "Define it"            
+
+find :: Bits -> Trie a -> a
+find [] (Leaf x) = x
+find [] (Node _ _) = error "No more bits but still at a node"
+find (F:bs) (Node l _) = find bs l
+find (T:bs) (Node _ r) = find bs r
+find _ (Leaf _) = error "No more bits to traverse"
+
+
+decode :: [Bit] -> Trie a -> [a]
+decode [] _ = []
+decode bits trie =
+    let (val, remainingBits) = decodeOne bits trie
+    in val : decode remainingBits trie
+
+decodeOne :: [Bit] -> Trie a -> (a, [Bit])
+decodeOne [] (Leaf x) = (x, [])
+decodeOne (F:bs) (Node left _) = decodeOne bs left
+decodeOne (T:bs) (Node _ right) = decodeOne bs right
+decodeOne bs (Leaf x) = (x, bs)
+decodeOne [] (Node _ _) = error "No bits left but still at a node"
+
+
+toList :: Trie a -> [(a, Bits)]
+toList = toList' []
+
+toList' :: Bits -> Trie a -> [(a, Bits)]
+toList' prefix (Leaf x) = [(x, prefix)]
+toList' prefix (Node l r) = toList' (prefix ++ [F]) l ++ toList' (prefix ++ [T]) r
